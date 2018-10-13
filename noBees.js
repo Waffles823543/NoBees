d330874898ab0e7eaaa878c115327430e57bf87a
Raf <rafael.deubler@gmail.com>
2018-10-13 18:33:44 +1100

    works

---
+++ noBees.js
@@ -0,0 +1,34 @@
+var Discord = require('discord.io');
+var logger = require('winston');
+var auth = require('./auth.json');
+
+// Configure logger settings
+logger.remove(logger.transports.Console);
+logger.add(new logger.transports.Console, {
+    colorize: true
+});
+logger.level = 'debug';
+
+// Initialize Discord Bot
+var bot = new Discord.Client({
+   token: auth.token,
+   autorun: true
+});
+
+bot.on('ready', function (evt) {
+    logger.info('Connected');
+    logger.info('Logged in as: ');
+    logger.info(bot.username + ' - (' + bot.id + ')');
+});
+
+bot.on('message', function (user, userID, channelID, message, evt) {
+    const bees = "(bee|bees)"
+    
+    if(message.match(bees) && userID != bot.id){
+        logger.info("bees");
+        bot.sendMessage({
+            to: channelID,
+            message: "no bees"
+        });
+    }
+});
