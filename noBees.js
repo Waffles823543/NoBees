db9c53bb180079b10697e4b7f782f56fc2ddb62b
Raf <rafael.deubler@gmail.com>
2018-10-16 20:59:47 +1100

    added persistance loop and fortnite thing

--- noBees.js
+++ noBees.js
@@ -7,6 +7,8 @@ var app = express();
 app.get('/', (req, res) => res.send('welcome to no bees website... it\'s a work in progress'))
 app.listen(process.env.PORT || 5000);
 
+const fortniteInfractions = {}
+
 // Configure logger settings
 logger.remove(logger.transports.Console);
 logger.add(new logger.transports.Console, {
@@ -29,14 +31,15 @@ bot.on('ready', function (evt) {
 bot.on('message', function (user, userID, channelID, message, evt) {
     const bees = "(bee|bees)"
     const noU = "(no u)"
+    const fortnite = "(fortnite)"
     
-    if(message.match(bees) && userID != bot.id){
+    if(message.toLowerCase().match(bees) && userID != bot.id){
         logger.info("bees");
         bot.sendMessage({
             to: channelID,
             message: "no bees"
         });
-    }else if(message.match(noU) && userID != bot.id){
+    }else if(message.toLowerCase().match(noU) && userID != bot.id){
         logger.info("no u");
         let chosenOne = bot.users[Object.keys(bot.users)[Math.floor(Math.random() * Object.keys(bot.users).length)]];
         while (chosenOne.bot == true){
@@ -49,5 +52,23 @@ bot.on('message', function (user, userID, channelID, message, evt) {
             to: channelID,
             message: "no <@" + chosenOne.id + ">"
         });
+    }else if(message.toLowerCase().match(fortnite) && userID != bot.id){
+        logger.info("fortnite");
+        if(fortniteInfractions[userID]){
+            fortniteInfractions[userID]++;
+        }else{
+            fortniteInfractions[userID] = 1;
+        }
+
+        if(fortniteInfractions[userID]+1 % 3 == 0){
+            bot.sendMessage({
+                to: channelID,
+                message: "ur mom gay"
+            });
+        }
     }
 });
+
+setInterval(() => {
+    console.log("100s");
+}, 100000)
